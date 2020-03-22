c88eee096f43647ac4983a0573107fc4250f7ac6
xiaoguoguo <1662106538@qq.com>
2020-03-22 23:21:41 +0800

    修改路由选中样式

--- src/conponents/Header/index.js
+++ src/conponents/Header/index.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState,useEffect } from 'react';
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
 import style from './style.css';
@@ -26,12 +26,12 @@ const Header = (props) => {
                 <div className={style.wrap}>
                     <h1 className={style.logo} style={{ backgroundImage: `url(${topbar})` }}></h1>
                     <div className={style.link}>
-                        <Link to='/' className={path === '/' ? style.focus : style.topNav}>发现音乐</Link>
-                        <Link to='/my' className={path === '/my' ? style.focus : style.topNav}>我的音乐</Link>
-                        <Link to='/friend' className={path === '/friend' ? style.focus : style.topNav}>朋友</Link>
-                        <Link to='/store' className={path === '/store' ? style.focus : style.topNav}>商城</Link>
-                        <Link to='/musician' className={path === '/musician' ? style.focus : style.topNav}>音乐人</Link>
-                        <Link to='/download' className={path === '/download' ? style.focus : style.topNav}>下载客户端</Link>
+                        <Link to='/' className={path === '/' ? style.focus : style.topNav} onClick={()=>{setPath('/')}}>发现音乐</Link>
+                        <Link to='/my' className={path === '/my' ? style.focus : style.topNav} onClick={()=>{setPath('/my')}}>我的音乐</Link>
+                        <Link to='/friend' className={path === '/friend' ? style.focus : style.topNav} onClick={()=>{setPath('/friend')}}>朋友</Link>
+                        <Link to='/store' className={path === '/store' ? style.focus : style.topNav} onClick={()=>{setPath('/store')}}>商城</Link>
+                        <Link to='/musician' className={path === '/musician' ? style.focus : style.topNav} onClick={()=>{setPath('/musician')}}>音乐人</Link>
+                        <Link to='/download' className={path === '/download' ? style.focus : style.topNav} onClick={()=>{setPath('/download')}}>下载客户端</Link>
                         <sup style={{ backgroundImage: `url(${topbar})` }}>&nbsp;</sup>
                     </div>
                     <div className={style.inputWarp} style={{ backgroundImage: `url(${topbar})` }}>
